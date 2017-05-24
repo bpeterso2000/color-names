4be814d8ebce26fe21c801e3ad8e3aa75b945a37
Brian Peterson <bpeterso2000@yahoo.com>
2017-05-24 19:27:30 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='color-names',
+    author='Brian Peterson',
+    author_email='bpeterso2000@yahoo.com',
+    version='1.0',
+    url='http://github.com/bpetreso2000/color-names',
+    packages=['color-names'],
+    description='A curated collection of color names to RGB hex code mappings',
+)
